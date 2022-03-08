df7a113b60f71bd0e8065b7caacff6338c6364f7
Kévin Dérécusson <derec84@hotmail.fr>
2022-03-08 14:42:41 +0100

    Display best movie title, image and resume

--- script/main.js
+++ script/main.js
@@ -1,32 +1,61 @@
-// let bestMovieImg = document.getElementById("best-movie-img")
-const best_imdb_score_filter_url = "http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains="
+const bestImdbScoreFilterUrl = "http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains="
 
 
-function getBestMovieUrl() {
-    return fetch(best_imdb_score_filter_url)
-        .then(response => response.json())
-        .then(data => {
-            let list_of_best_movies = getListOfBestMovie(data)
-            if (list_of_best_movies.length === 0){
-                let best_movie_url = list_of_best_movies[0].url
-            }
-            else {
-                let best_movie_url = getBestMovieByVotes(list_of_best_movies)
-            }
-            return best_movie_url
-        })
+async function manageBestMovie() {
+    const response = await fetch(bestImdbScoreFilterUrl)
+    const data = await response.json()
+    
+    let listOfBestMovies = getListOfBestMovie(data)
+    if (listOfBestMovies.length === 1){
+        let bestMovieUrl = listOfBestMovies[0].url
+        displayBestMovieInformation(bestMovieUrl)
+    }
+    else {
+        let bestMovieUrl = getBestMovieByVotes(listOfBestMovies)
+        displayBestMovieInformation(bestMovieUrl)
+    }
 }
 
 
 function getListOfBestMovie(data){
-    let list_of_best_movies = []
-    let max_imbd_score = -1
+    let listOfBestMovies = []
+    let maxImdbScore = -1
     for (let pas = 0; pas < data.results.length; pas++){
-        let imdb_score = data.results[pas].imdb_score
-        if (max_imbd_score <= imdb_score){
-            max_imbd_score = imdb_score
-            list_of_best_movies.push(data.results[pas])
+        let imdbScore = data.results[pas].imdb_score
+        if (maxImdbScore <= imdbScore){
+            maxImdbScore = imdbScore
+            listOfBestMovies.push(data.results[pas])
         }
+    }
+    return listOfBestMovies
+}
+
+
+function getBestMovieByVotes(listOfBestMovies){
+    let maxVotes = -1
+    for (let pas = 0; pas < listOfBestMovies.length; pas++){
+        let votes = listOfBestMovies[pas].votes
+        if (maxVotes <= votes){
+            maxVotes = votes
+            bestMovieUrl = listOfBestMovies[pas].url
+        }
+    }
+    return bestMovieUrl
+}
+
+
+async function displayBestMovieInformation(bestMovieUrl){
+    let bestMovieTitle = document.getElementById("the-best-movie").getElementsByTagName("h1")[0]
+    let bestMovieResume = document.getElementById("the-best-movie").getElementsByTagName("p")[0]
+    let bestMovieImg = document.getElementById("best-movie-img")
+
+    const response = await fetch(bestMovieUrl)
+    const data = await response.json()
+    bestMovieTitle.innerHTML = data.title
+    bestMovieResume.innerHTML = data.description
+    bestMovieImg.src = data.image_url  
+}
+
     }
     return list_of_best_movies
 }
@@ -41,9 +70,10 @@ function getBestMovieByVotes(list_of_best_movies){
             best_movie_url = list_of_best_movies[pas].url
         }
     }
-    return best_movie_url
+
+async function main(){
+    await manageBestMovie()
 }
 
 
-let best_movie_url = getBestMovieUrl()
-best_movie_url.then(result => console.log(result))
+main()
