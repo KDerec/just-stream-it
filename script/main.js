0f84073731b0718ea65ab0deef36dca1a1e1d4af
Kévin Dérécusson <derec84@hotmail.fr>
2022-03-10 19:01:01 +0100

    Display images from API of top seven best movies

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 const bestImdbScoreFilterUrl = "http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains="
 const genreListUrl = "http://localhost:8000/api/v1/genres/"
 const genreList = []
-
+const urlListOfTopSevenBestMovies = []
 
 async function manageBestMovie() {
     const response = await fetch(bestImdbScoreFilterUrl)
@@ -13,7 +13,7 @@ async function manageBestMovie() {
         displayBestMovieInformation(bestMovieUrl)
     }
     else {
-        let bestMovieUrl = getBestMovieByVotes(listOfBestMovies)
+        let bestMovieUrl = getTheBestMovieByVotes(listOfBestMovies)
         displayBestMovieInformation(bestMovieUrl)
     }
 }
@@ -33,7 +33,7 @@ function getListOfBestMovie(data){
 }
 
 
-function getBestMovieByVotes(listOfBestMovies){
+function getTheBestMovieByVotes(listOfBestMovies){
     let maxVotes = -1
     for (let pas = 0; pas < listOfBestMovies.length; pas++){
         let votes = listOfBestMovies[pas].votes
@@ -59,18 +59,61 @@ async function displayBestMovieInformation(bestMovieUrl){
 }
 
 
-async function getNextPageOfGenreList(url){
+async function manageTopSevenBestMovies() {
+    let response = await fetch(bestImdbScoreFilterUrl)
+    let data = await response.json()
+    let urlListOfTopSevenBestMovies = await getUrlListOfTopSevenBestMovies(data)
+    for (pas = 0; pas < 7; pas++){
+        let response = await fetch(urlListOfTopSevenBestMovies[pas])
+        let data = await response.json()
+        await displayTopSevenBestMovies(data.image_url)
+
+}
+
+
+async function displayTopSevenBestMovies(image_url) {
+    let node = document.createElement("li")
+    let imageNode = document.createElement("img")
+    imageNode.src = image_url
+    imageNode.className = "carousel-item"
+    node.appendChild(imageNode)
+    document.getElementById("carousel-items").appendChild(node)
+    }
+}
+
+
+async function getUrlListOfTopSevenBestMovies(data) {
+    for (let pas = 0; pas < data.results.length; pas++){
+        urlListOfTopSevenBestMovies.push(data.results[pas].url)
+    }
+    if (urlListOfTopSevenBestMovies.length < 7){
+        let next_page_data = await getNextPageData(data.next)
+        getUrlListOfTopSevenBestMovies(next_page_data)
+    }
+    return urlListOfTopSevenBestMovies
+}
+
+
+async function getNextPageData(url){
+    let response = await fetch(url)
+    let data = await response.json()
+
+    return data
+}
+
+
+async function getNextPageOfGenreList(url) {
     const response = await fetch(url)
     const data = await response.json()
     
     if (data.next){
-        await getAllGenreOfThePage(data.next)
+        await getAllGenreNameOfThePage(data.next)
         await getNextPageOfGenreList(data.next)
     }
 }
 
 
-async function getAllGenreOfThePage(url){
+async function getAllGenreNameOfThePage(url){
     const response = await fetch(url)
     const data = await response.json()
     for (pas = 0; pas < data.results.length; pas++){
@@ -79,7 +122,7 @@ async function getAllGenreOfThePage(url){
 }
 
 
-function createGenreList(){
+function appendAllGenreInCategorieList(){
     for (pas = 0; pas < genreList.length; pas++){
         let node = document.createElement("li")
         let textNode = document.createTextNode(genreList[pas])
@@ -90,19 +133,21 @@ function createGenreList(){
 
 async function main(){
     await manageBestMovie()
-    await getAllGenreOfThePage(genreListUrl)
+    await getAllGenreNameOfThePage(genreListUrl)
     await getNextPageOfGenreList(genreListUrl)
-    createGenreList()
+    appendAllGenreInCategorieList()
+    await manageTopSevenBestMovies()
 }
 
 const buttonRight = document.getElementById('slideRight');
 const buttonLeft = document.getElementById('slideLeft');
 
 buttonRight.onclick = function () {
-  document.getElementById('carousel-items').scrollLeft += 400;
+  document.getElementById('carousel-items').scrollLeft += 200;
 };
 buttonLeft.onclick = function () {
-  document.getElementById('carousel-items').scrollLeft -= 400;
+  document.getElementById('carousel-items').scrollLeft -= 200;
 };
+
 
 main()
