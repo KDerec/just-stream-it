8ed9de26041655ea0630d36e05079d28eed05f79
Kévin Dérécusson <derec84@hotmail.fr>
2022-03-14 10:58:52 +0100

    No cache for the localserver

--- server.py
+++ server.py
@@ -1,27 +1,37 @@
-# -*- coding: utf-8 -*-
-#test on python 3.4 ,python of lower version  has different module organization.
 import http.server
-from http.server import HTTPServer, BaseHTTPRequestHandler
 import socketserver
+
 
 PORT = 8080
 
 Handler = http.server.SimpleHTTPRequestHandler
 
 Handler.extensions_map={
-        '.manifest': 'text/cache-manifest',
+    '.manifest': 'text/cache-manifest',
 	'.html': 'text/html',
-        '.png': 'image/png',
+    '.png': 'image/png',
 	'.jpg': 'image/jpg',
 	'.svg':	'image/svg+xml',
 	'.css':	'text/css',
 	'.js':	'text/javascript',
-        '.json': 'application/json',
-        '.xml': 'application/xml',
+    '.json': 'application/json',
+    '.xml': 'application/xml',
 	'': 'application/octet-stream', # Default
     }
 
-httpd = socketserver.TCPServer(("", PORT), Handler)
+
+class MyHTTPRequestHandler(Handler):
+    def end_headers(self):
+        self.send_my_headers()
+        Handler.end_headers(self)
+
+    def send_my_headers(self):
+        self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+
+
+httpd = socketserver.TCPServer(("", PORT), MyHTTPRequestHandler)
 
 print("serving at port", PORT)
 httpd.serve_forever()
