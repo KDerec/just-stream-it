1f8dc18a21d1f4e209e0d0662aac5a231d5fd512
Kévin Dérécusson <derec84@hotmail.fr>
2022-03-16 16:58:55 +0100

    Improve the search function for the best movie

--- scripts/theBestMovie.js
+++ scripts/theBestMovie.js
@@ -1,6 +1,7 @@
 import {fetchDataFromUrl} from './dataFetcher.js'
 import {createCarouselItemFromUrl} from './carousel.js'
 
+
 export async function manageTheBestMovie(id) {
     let theBestMovieUrl = await getTheBestMovieUrl()
     displayBestMovieInformation(theBestMovieUrl, id)
@@ -8,46 +9,121 @@ export async function manageTheBestMovie(id) {
 
 
 export async function getTheBestMovieUrl() {
-    let bestOfAllMovieUrl = "http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains="
-    let listOfBestMovies = await getListOfBestMovies(bestOfAllMovieUrl)
-    if (listOfBestMovies.length === 1){
-        let theBestMovieUrl = listOfBestMovies[0].url
+    let bestOfAllMovieUrlPage = "http://localhost:8000/api/v1/titles/?year=&min_year=&max_year=&imdb_score=&imdb_score_min=&imdb_score_max=&title=&title_contains=&genre=&genre_contains=&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains="
+    let UrlListOfBestMovies = await getUrlListOfThePage(bestOfAllMovieUrlPage)
+    UrlListOfBestMovies = await getUrlListOfBestMoviesByImdbScore(UrlListOfBestMovies)
+    if (UrlListOfBestMovies.length == 1){
+        let theBestMovieUrl = UrlListOfBestMovies[0].url
 
         return theBestMovieUrl
     }
     else {
-        let theBestMovieUrl = getTheBestMovieByVotes(listOfBestMovies)
+        UrlListOfBestMovies = await getUrlListOfBestMovieByAvgVote(UrlListOfBestMovies)
+        if (UrlListOfBestMovies.length == 1){
+            let theBestMovieUrl = UrlListOfBestMovies[0]
 
-        return theBestMovieUrl
+            return theBestMovieUrl
+    
+        } else {
+            UrlListOfBestMovies = await getUrlListOfBestMovieByVotes(UrlListOfBestMovies)
+            if (UrlListOfBestMovies.length == 1){
+                let theBestMovieUrl = UrlListOfBestMovies[0]
+        
+                return theBestMovieUrl
+        
+            } else {
+                let rand = Math.floor(Math.random()*UrlListOfBestMovies.length);
+                let theBestMovieUrl = UrlListOfBestMovies[rand]
+        
+                return theBestMovieUrl
+            }
+        } 
     }
 }
 
 
-async function getListOfBestMovies(url){
+async function getUrlListOfThePage(url){
+    let UrlListOfBestMovies = []
     let data = await fetchDataFromUrl(url)
-    let listOfBestMovies = []
-    let maxImdbScore = -1
-    for (let pas = 0; pas < data.results.length; pas++){
-        let imdbScore = data.results[pas].imdb_score
-        if (maxImdbScore <= imdbScore){
-            maxImdbScore = imdbScore
-            listOfBestMovies.push(data.results[pas])
-        }
+    for(let pas = 0; pas < data.results.length; pas++){
+        UrlListOfBestMovies.push(data.results[pas].url)
     }
-    return listOfBestMovies
+    return UrlListOfBestMovies
 }
 
 
-function getTheBestMovieByVotes(listOfBestMovies){
-    let maxVotes = -1
-    for (let pas = 0; pas < listOfBestMovies.length; pas++){
-        let votes = listOfBestMovies[pas].votes
-        if (maxVotes <= votes){
-            maxVotes = votes
-            var theBestMovieUrl = listOfBestMovies[pas].url
+async function getUrlListOfBestMoviesByImdbScore(UrlListOfBestMovies){
+    let maxImdbScore = -1
+    let listOfBestMoviesByImdbScore = []
+    for (let pas = 0; pas < UrlListOfBestMovies.length; pas++){
+        let data = await fetchDataFromUrl(UrlListOfBestMovies[pas])
+        if (listOfBestMoviesByImdbScore.length == 0 & maxImdbScore == -1){
+            listOfBestMoviesByImdbScore.push(data.url)
+            maxImdbScore = data.imdb_score
+        }
+        if (maxImdbScore < data.imdb_score){
+            listOfBestMoviesByImdbScore = []
+            listOfBestMoviesByImdbScore.push(data.url)
+            maxImdbScore = data.imdb_score
+        }
+        if (maxImdbScore == data.imdb_score){
+            listOfBestMoviesByImdbScore.push(data.url)
         }
     }
-    return theBestMovieUrl
+
+    listOfBestMoviesByImdbScore = [...new Set(listOfBestMoviesByImdbScore)]
+
+    return listOfBestMoviesByImdbScore
+}
+
+
+async function getUrlListOfBestMovieByAvgVote(UrlListOfBestMovies){
+    let maxAvgVote = -1
+    let listOfBestMoviesByAvg = []
+    for (let pas = 0; pas < UrlListOfBestMovies.length; pas++){
+        let data = await fetchDataFromUrl(UrlListOfBestMovies[pas])
+        if (listOfBestMoviesByAvg.length == 0 & maxAvgVote == -1){
+            listOfBestMoviesByAvg.push(data.url)
+            maxAvgVote = data.avg_vote
+        }
+        if (maxAvgVote < data.avg_vote){
+            listOfBestMoviesByAvg = []
+            listOfBestMoviesByAvg.push(data.url)
+            maxAvgVote = data.avg_vote
+        }
+        if (maxAvgVote == data.avg_vote){
+            listOfBestMoviesByAvg.push(data.url)
+        }
+    }
+
+    listOfBestMoviesByAvg = [...new Set(listOfBestMoviesByAvg)]
+    
+    return listOfBestMoviesByAvg
+}
+
+
+async function getUrlListOfBestMovieByVotes(UrlListOfBestMovies){
+    let maxVotes = -1
+    let listOfBestMoviesByVotes = []
+    for (let pas = 0; pas < UrlListOfBestMovies.length; pas++){
+        let data = await fetchDataFromUrl(UrlListOfBestMovies[pas])
+        if (listOfBestMoviesByVotes.length == 0 & maxVotes == -1){
+            listOfBestMoviesByVotes.push(data.url)
+            maxVotes = data.votes
+        }
+        if (maxVotes < data.votes){
+            listOfBestMoviesByVotes = []
+            listOfBestMoviesByVotes.push(data.url)
+            maxVotes = data.votes
+        }
+        if (maxVotes == data.votes){
+            listOfBestMoviesByVotes.push(data.url)
+        }
+    }
+
+    listOfBestMoviesByVotes = [...new Set(listOfBestMoviesByVotes)]
+
+    return listOfBestMoviesByVotes
 }
 
 
