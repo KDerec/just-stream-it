4de4b11775c1fb10b35941c3e3f844fc80bd34c3
Kévin Dérécusson <derec84@hotmail.fr>
2022-03-21 21:16:48 +0100

    Simplify the data fetcher

--- scripts/dataFetcher.js
+++ scripts/dataFetcher.js
@@ -1,6 +1,3 @@
 export async function fetchDataFromUrl(url) {
-    let response = await fetch(url);
-    let data = await response.json();
-
-    return data;
+    return (await fetch(url)).json();
 }
