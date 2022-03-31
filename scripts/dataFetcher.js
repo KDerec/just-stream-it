fa9fd8ed0f9e770a448575ae173e67a0cb00ecd4
Kévin Dérécusson <derec84@hotmail.fr>
2022-03-31 10:20:29 +0200

    Handle errors for fetch function

--- scripts/dataFetcher.js
+++ scripts/dataFetcher.js
@@ -1,3 +1,15 @@
 export async function fetchDataFromUrl(url) {
-    return (await fetch(url)).json();
+    try {
+        let response = await fetch(url);
+        if (response.ok) {
+            let data = await response.json();
+            return data;
+        } else {
+            let message = 'Le code de statut de réponse HTTP est ' + response.status;
+            throw new Error(message);
+        }    
+    }
+    catch(error) {
+        console.log('Il y a eu un problème avec l\'opération fetch : ' + error.message);
+    }
 }
